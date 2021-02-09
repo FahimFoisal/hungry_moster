fb22b07cbcc365d4b35ac19f5bf97f843c8c058c
Fahim Foisal <fahimuschas@gmail.com>
2021-02-09 23:28:24 +0600

    Final assignment submission

--- api.js
+++ api.js
@@ -3,6 +3,8 @@ function capturingSearchItem () {
     parsingInfo(searchMenu[0]);
     const result = document.getElementById("ingredient");
     result.innerHTML = '';
+    const itemNotFound = document.getElementById('notFound');
+    itemNotFound.innerHTML = "";
 }
 
 function parsingInfo(searchItem) {
@@ -11,10 +13,14 @@ function parsingInfo(searchItem) {
     .then (data => {
         if (data.meals == null) {
             console.log("menu does not found");
+            const itemNotFound = document.getElementById('notFound');
+            const paragraph =`
+                <h2>Menu Not Found</h2>
+            `;
+            itemNotFound.innerHTML = paragraph;
+
         }
         else {
-            // console.log(data.meals[0].strMeal);
-            console.log(data.meals);
             creatingSearchResult(data.meals);
         }
     });
@@ -24,7 +30,6 @@ function creatingSearchResult (arrayItems) {
     const result = document.getElementById("searchResult");
     result.innerHTML = '';
     arrayItems.forEach(item => {
-        
         const divBlock = document.createElement('div');
         divBlock.className = 'divBlock';
         const htmlTag = `
@@ -32,39 +37,36 @@ function creatingSearchResult (arrayItems) {
         <h3>${item.strMeal}</h3>
         `;
         divBlock.innerHTML = htmlTag;
-        result.appendChild(divBlock);
-        
-        
+        result.appendChild(divBlock);   
     });
-    const clicked = document.getElementById('searchResult');
-        clicked.addEventListener('click',function(event) {
-            console.log(event.target.getAttribute('src'));
-            for (let i = 0; i < arrayItems.length; i++) {
-                if (arrayItems[i].strMealThumb == event.target.getAttribute('src') || arrayItems[i].strMeal == event.target.innerText) {
-                    const ingredients = document.getElementById("ingredient");
-                    ingredients.innerHTML = '';
-                    const foodDetails = `
-                        <img src=${arrayItems[i].strMealThumb} width='400px'>
-                        <h1>${arrayItems[i].strMeal}</h1>
-                        <h3>Ingredients</h3>
-                    `;
-                    ingredients.innerHTML = foodDetails;
-                    const ingredientUl = document.createElement('ul');
-                    ingredientUl.className = 'ingredientList';
-                        const ingredientsItem = `
-                            <li>${arrayItems[i].strMeasure1} ${arrayItems[i].strIngredient1}</li>
-                            <li>${arrayItems[i].strMeasure2} ${arrayItems[i].strIngredient2}</li>
-                            <li>${arrayItems[i].strMeasure3} ${arrayItems[i].strIngredient3}</li>
-                            <li>${arrayItems[i].strMeasure4} ${arrayItems[i].strIngredient4}</li>
-                            <li>${arrayItems[i].strMeasure5} ${arrayItems[i].strIngredient5}</li>
-                            <li>${arrayItems[i].strMeasure6} ${arrayItems[i].strIngredient6}</li>
-                        `;
-                        ingredientUl.innerHTML = ingredientsItem;
-                    ingredients.appendChild(ingredientUl);
-                    break; 
-                }   
-            }
-        })
+const clicked = document.getElementById('searchResult');
+    clicked.addEventListener('click',function(event) {
+        for (let i = 0; i < arrayItems.length; i++) {
+            if (arrayItems[i].strMealThumb == event.target.getAttribute('src') || arrayItems[i].strMeal == event.target.innerText) {
+                const ingredients = document.getElementById("ingredient");
+                ingredients.innerHTML = '';
+                const foodDetails = `
+                    <img src=${arrayItems[i].strMealThumb} width='400px'>
+                    <h1>${arrayItems[i].strMeal}</h1>
+                    <h3>Ingredients</h3>
+                `;
+                ingredients.innerHTML = foodDetails;
+                const ingredientUl = document.createElement('div');
+                ingredientUl.className = 'ingredientList';
+                const ingredientsItem = `
+                    <p>${arrayItems[i].strMeasure1} ${arrayItems[i].strIngredient1}</p>
+                    <p>${arrayItems[i].strMeasure2} ${arrayItems[i].strIngredient2}</p>
+                    <p>${arrayItems[i].strMeasure3} ${arrayItems[i].strIngredient3}</p>
+                    <p>${arrayItems[i].strMeasure4} ${arrayItems[i].strIngredient4}</p>
+                    <p>${arrayItems[i].strMeasure5} ${arrayItems[i].strIngredient5}</p>
+                    <p>${arrayItems[i].strMeasure6} ${arrayItems[i].strIngredient6}</p>
+                `;
+                ingredientUl.innerHTML = ingredientsItem;
+                ingredients.appendChild(ingredientUl);
+                break; 
+            }   
+        }
+    })
 }
 
 
