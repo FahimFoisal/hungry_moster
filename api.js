c3e18c2d5e31ad102d7a4fa1b82d16de6744b76d
Fahim Foisal <fahimuschas@gmail.com>
2021-02-09 09:44:48 +0600

    this is not final upload

--- api.js
+++ api.js
@@ -1,6 +1,8 @@
 function capturingSearchItem () {
     const searchMenu = document.getElementById('menuItemSearch').value;
     parsingInfo(searchMenu[0]);
+    const result = document.getElementById("ingredient");
+    result.innerHTML = '';
 }
 
 function parsingInfo(searchItem) {
@@ -12,7 +14,7 @@ function parsingInfo(searchItem) {
         }
         else {
             // console.log(data.meals[0].strMeal);
-            // console.log(data.meals);
+            console.log(data.meals);
             creatingSearchResult(data.meals);
         }
     });
@@ -20,6 +22,7 @@ function parsingInfo(searchItem) {
 
 function creatingSearchResult (arrayItems) {
     const result = document.getElementById("searchResult");
+    result.innerHTML = '';
     arrayItems.forEach(item => {
         
         const divBlock = document.createElement('div');
@@ -30,16 +33,39 @@ function creatingSearchResult (arrayItems) {
         `;
         divBlock.innerHTML = htmlTag;
         result.appendChild(divBlock);
-        // divBlock.setAttribute("onclick", `menuIngredient(${})`)
-        // console.log(item.strMealThumb);
-
+        
+        
     });
+    const clicked = document.getElementById('searchResult');
+        clicked.addEventListener('click',function(event) {
+            console.log(event.target.getAttribute('src'));
+            for (let i = 0; i < arrayItems.length; i++) {
+                if (arrayItems[i].strMealThumb == event.target.getAttribute('src') || arrayItems[i].strMeal == event.target.innerText) {
+                    const ingredients = document.getElementById("ingredient");
+                    ingredients.innerHTML = '';
+                    const foodDetails = `
+                        <img src=${arrayItems[i].strMealThumb} width='400px'>
+                        <h1>${arrayItems[i].strMeal}</h1>
+                        <h3>Ingredients</h3>
+                    `;
+                    ingredients.innerHTML = foodDetails;
+                    const ingredientUl = document.createElement('ul');
+                    ingredientUl.className = 'ingredientList';
+                        const ingredientsItem = `
+                            <li>${arrayItems[i].strMeasure1} ${arrayItems[i].strIngredient1}</li>
+                            <li>${arrayItems[i].strMeasure2} ${arrayItems[i].strIngredient2}</li>
+                            <li>${arrayItems[i].strMeasure3} ${arrayItems[i].strIngredient3}</li>
+                            <li>${arrayItems[i].strMeasure4} ${arrayItems[i].strIngredient4}</li>
+                            <li>${arrayItems[i].strMeasure5} ${arrayItems[i].strIngredient5}</li>
+                            <li>${arrayItems[i].strMeasure6} ${arrayItems[i].strIngredient6}</li>
+                        `;
+                        ingredientUl.innerHTML = ingredientsItem;
+                    ingredients.appendChild(ingredientUl);
+                    break; 
+                }   
+            }
+        })
 }
 
-// function menuIngredient (arg) {
-//     const clickedMenu = document.getElementsByClass('divBlock');
-//     clickedMenu.addEventListener('click', function () {
 
-//     })
-// }
 
